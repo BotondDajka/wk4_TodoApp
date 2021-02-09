9f4306fe5d2fa1ec0a8401b852282503f61adc7f
Botond <botond.dajka@tassomai.com>
2021-02-09 12:16:22 +0000

    drag and drop makes api call

--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
 const container = document.getElementById('container')
+let boardId = null;
 
 function generateBoard(id) {
     document.getElementById('container').innerHTML = ""
     $.get(`http://localhost:3000/api/board/${id}`, function(data) {
-        console.log(data)
+        boardId = data.id
         document.getElementById('boardName').value = data.name
         for(let i = 0; i < data.areas.length; i++) {
             const currentArea = data.areas[i]
             const column = document.createElement('div')
             column.classList.add('column')
+            column.id = "column"+currentArea.id
 
             const columnTitle = document.createElement('textarea')
             columnTitle.innerHTML = currentArea.title
@@ -27,6 +29,7 @@ function generateBoard(id) {
                 const currentTask = currentArea.tasks[j]
                 const li = document.createElement('li')
                 li.classList.add('task')
+                li.id = "task"+currentTask.id
 
                 const taskTitle = document.createElement('textarea')
                 taskTitle.classList.add('edit')
@@ -51,12 +54,36 @@ function generateBoard(id) {
         }
         $( function() {
             $( ".connectedSortable, .connectedSortable" ).sortable({
-              connectWith: ".connectedSortable",
-              dropOnEmpty: true
-            }).disableSelection();
-          } );
+                connectWith: ".connectedSortable",
+                dropOnEmpty: true,
+                receive: function( event, ui ) {
+                    console.log(ui.item["0"])
+                    console.log(lastParent.id)
 
-          $(".edit").on("keydown keyup click", function(){
+                    const itemId = ui.item["0"].id.replace("task","")
+                    const oldColumnId = lastParent.id.replace("column","")
+                    const newColumnId = ui.item["0"].parentNode.parentNode.id.replace("column","")
+
+                    console.log(newColumnId)
+                    
+                    $.ajax({
+                        type: "POST",
+                        url: `http://localhost:3000/api/board/${boardId}/area/${oldColumnId}/task/${itemId}/move`,
+                        // The key needs to match your method's input parameter (case-sensitive).
+                        data: JSON.stringify({ "columnId" : newColumnId }),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                    });
+
+                },
+                start: function( event, ui ) {   
+                    lastParent = this.parentNode
+                }
+            
+            }).disableSelection();
+        } );
+
+        $(".edit").on("keydown keyup click", function(){
             this.style.height = "1px";
             this.style.height = (this.scrollHeight) + "px"; 
         });
